1991b7c5f73464110154392024dd2430f5324ebb
DinaraK <iamnotmugle@gmail.com>
2023-06-07 03:20:30 +0600

    v3

--- fibonachi.cpp
+++ fibonachi.cpp
@@ -1,16 +1,24 @@
-﻿#include<iostream>
-#include<cmath>
+﻿#include <iostream>
 using namespace std;
 
-int fib(int n) {
-	double phi = (1 + sqrt(5)) / 2;
-	return round(pow(phi, n) / sqrt(5));
+int fibonacci(int n)
+{
+	if (n <= 1)
+		return n;
+	int prev = 0;
+	int curr = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		int temp = curr;
+		curr = prev + curr;
+		prev = temp;
+	} return curr;
 }
 
 int main()
 {
 	int n;
 	cin >> n;
-	cout << fib(n) << std::endl;
+	cout << fibonacci(n) << endl;
 	return 0;
 }
