9acb60eef9d973c387f5fa16e68d8a5780848d57
DinaraK <iamnotmugle@gmail.com>
2023-06-07 03:19:22 +0600

    v1.cpp

--- fibonachi.cpp
+++ fibonachi.cpp
@@ -1,24 +1,17 @@
 ﻿#include <iostream>
-using namespace std; 
+using namespace std;
 
-int fibonacci(int n) 
-{ 
-	if (n <= 1) 
-		return n; 
-	int prev = 0; 
-	int curr = 1; 
-	for (int i = 2; i <= n; i++) 
-	{ 
-		int temp = curr; 
-		curr = prev + curr; 
-		prev = temp; 
-	} return curr; 
-} 
+int fibonachi(int n)
+{
+	if (n <= 1)
+		return n;
+	return fibonachi(n - 1) + fibonachi(n - 2);
+}
 
-int main() 
-{ 
+int main()
+{
 	int n;
 	cin >> n;
-	cout << fibonacci(n) << endl;
+	cout << fibonachi(n) << endl;
 	return 0;
 }
